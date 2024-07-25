7487a6208e92674e91e138c51c642c352d69e7cc
Kamil Novák <kamil.novak86@gmail.com>
2024-07-25 16:57:27 +0200

    Upravena šířka panelu formuláře a velikost karet typu závady ve formuláři.

--- config/default.js
+++ config/default.js
@@ -63,7 +63,7 @@ var config = {
     ],
  
     // Obsah widgetu o aplikaci (HTML)
-    infoWidgetContent: "<div class='about-widget'><h3>Hlášení závad ve městě</h3><div><h4>Autoři</h4><p><a href='https://jihlava.cz' target='_blank'>Statutární město Jihlava</a><br>odbor informatiky<br>gis@jihlava-city.cz<br>Verze 1.0</p></div></div>",
+    infoWidgetContent: "<div class='about-widget'><h3>Hlášení závad ve městě</h3><div><h4>Autoři</h4><p><a href='https://jihlava.cz' target='_blank'>Statutární město Jihlava</a><br>odbor informatiky<br>gis@jihlava-city.cz<br>Verze 1.0.1</p></div></div>",
 
     // Maximální rozměry a kvalita přílohy
     // quality = zadávat v rozmezí 0-1 (např. 0.75)
